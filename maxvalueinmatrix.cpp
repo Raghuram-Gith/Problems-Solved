04af970a6c32a27f6b281d81b78459163c47aa38
P.Raghuram <puttaraghuram2@gmail.com>
2026-06-08 20:52:22 +0530

    Create maxvalueinmatrix.cpp

---
+++ maxvalueinmatrix.cpp
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class MaximumValuesInAMatrix {
+    public static void main (String [] args ) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = Integer.MIN_VALUE; 
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int currentVal = sc.nextInt();
+                if (currentVal > max) {
+                    max = currentVal;
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
