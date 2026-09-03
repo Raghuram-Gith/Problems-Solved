3185aa37d6c1868cfdb369bd72d570c6936cfb67
P.Raghuram <puttaraghuram2@gmail.com>
2026-09-04 01:06:11 +0530

    Add E- Adjusting Drones.cpp for drone adjustment logic

---
+++ E- Adjusting Drones.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+#define ll long long int
+
+void solve() {
+    int N, K, curr;
+    cin >> N >> K;
+    
+    vector<int> cnt(N + N + N + 1, 0);
+    for (int i = 0; i < N; i++) {
+        cin >> curr;
+        cnt[curr]++;
+    }
+    
+    int ans = 0;
+    for (int val = 1; val <= N + N; val++) {
+        int curr = val;
+        while (cnt[curr] > K) {
+            cnt[curr + 1] += cnt[curr] - 1;
+            cnt[curr] = 1;
+            curr++;
+        }
+        ans = max(ans, curr - val);
+    }
+    
+    cout << ans << endl;
+}
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+    
+    return 0;
+}
