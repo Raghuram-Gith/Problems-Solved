ce33e8b6089c2dd6f686a6a75cbffbcb8c80edef
P.Raghuram <puttaraghuram2@gmail.com>
2026-09-07 09:49:50 +0530

    Modify main to handle multiple test cases

--- A-Odd Eraser.cpp
+++ A-Odd Eraser.cpp
@@ -21,7 +21,10 @@ void solve() {
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    solve();
+    int t;
+    cin>>t;
+    while(t--){
+    	solve();
+    }
     return 0;
 }
