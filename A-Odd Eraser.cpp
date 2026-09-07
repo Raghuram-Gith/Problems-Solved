914c3aedeac288661b01370ddc5c0cc2c18ce14a
P.Raghuram <puttaraghuram2@gmail.com>
2026-09-07 09:35:30 +0530

    Add A-Odd Eraser.cpp to calculate HCF

---
+++ A-Odd Eraser.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int hcf(int a, int b){
+	return b==0? a:hcf(b,a%b);
+}
+void solve() {
+    int N; cin>>N;
+    vector<int> arr(N);
+    for(int i=0;i<N;i++){
+    	cin>arr[i];
+    	}
+    	cout<<hcf(arr[0],arr[N-1])<<endl;
+    }
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+    return 0;
+}
